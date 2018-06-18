03a667d301ce12712730f5cc0bac3bb125d55e43
Cara <cararbrennan@gmail.com>
2018-06-18 15:05:37 -0700

    initial React Router functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
+      <Router>
+        <div>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -14,6 +17,11 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+      <Route path ="/" render={() => (
+        <h1>Welcome to your local Video Store</h1>
+      )} />
+    </div>
+    </Router>
     );
   }
 }
