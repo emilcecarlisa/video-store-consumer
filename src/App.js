99402682fb2a3dd88c445a8b12ecd047f6911043
emilcecarlisa <emilcecarlisa@gmail.com>
2018-06-18 15:59:59 -0700

    Added more React routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import Search from './components/Search'
 
 class App extends Component {
   render() {
+    const custId = ({ match }) => (
+      <div>
+      <Route path ={match.url + '/:id'} render={() => (
+        <h1>Customer #11111</h1>
+      )} />
+      </div>
+    )
+
     return (
       <Router>
         <div>
@@ -20,6 +29,17 @@ class App extends Component {
       <Route path ="/" render={() => (
         <h1>Welcome to your local Video Store</h1>
       )} />
+      <Route path ="/search" render={() => (
+        <h1>Search Movies</h1>
+      )} />
+      <Route path ="/library" render={() => (
+        <h1>Library</h1>
+      )} />
+      <Route path ="/customers" render={() => (
+        <h1>Customers</h1>
+      )} />
+
+      {custId}
     </div>
     </Router>
     );
---
+++ src/components/Search.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class Search extends Component {
+  render() {
+    return(
+      <h3>Search</h3>
+    )
+  }
+
+}
+
+export default Search;
