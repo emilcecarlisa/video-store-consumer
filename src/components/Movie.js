16b38ff2d57ddf2998424ec85f552c43cacc0346
Cara <cararbrennan@gmail.com>
2018-06-19 10:41:30 -0700

    some movie search functionality

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-
 class Movie extends Component {
+  constructor() {
+    super();
+
+    this.state ={
+      moviename: '',
+    };
+  }
+
+ onInputChange = (event) => {
+let updatedInput = {};
+updatedInput[event.target.name] =
+event.target.value;
+this.setState(updatedInput);
+ }
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+    this.props.getMoviesCallback(this.state);
+
+    this.setState({
+      moviename:'',
+    });
+  }
+
   render() {
 
     return(
       <section>
-        <h3>Search All Movies</h3>
-        <form>
-          <input type="text">
-
-          </input>
-          <label></label>
+        <form onSubmit = {this.onFormSubmit}>
+          <div>
+            <label htmlFor = "moviename">What Movie are you looking for?</label>
+            <input type="text"
+              name = "moviename"
+              value = {this.state.moviename}
+              onChange={this.onInputChange}
+            />
+          </div>
+          <button type="submit">Submit</button>
         </form>
-        <button>Submit</button>
       </section>
     )
   }
