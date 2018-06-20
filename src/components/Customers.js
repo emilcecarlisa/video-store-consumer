d14b60d1c079528febc7558220d6034b90022b2a
emilcecarlisa <emilcecarlisa@gmail.com>
2018-06-20 13:47:58 -0700

    Added rental button on customer page

--- src/components/Customers.js
+++ src/components/Customers.js
@@ -2,6 +2,24 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Customers extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      moviesCheckedOut: 0
+    };
+  }
+
+  movieSelected = (event) => {
+    // console.log(`THIS IS EVENT`,`${event.target.value}`);
+
+    console.log(`before reset state`,`${this.state.moviesCheckedOut.typeOf}`)
+
+    this.setState({moviesCheckedOut: this.state.moviesCheckedOut + 1});
+
+    console.log('after reset state', `${this.state.moviesCheckedOut}`)
+  }
+
   render() {
     return (
 
@@ -9,7 +27,10 @@ class Customers extends Component {
       {this.props.customers.map((customer, index)=>{
       console.log('customer', customer)
 
-      return <h1 key={customer.id}>{customer.name}</h1>
+      return <div key={index}>
+      <h1 key={customer.id}>{customer.name}</h1>
+      <button key={index} value={customer.name} onClick={this.movieSelected}>Select for Rental</button>
+      </div>
       })
       }
     </div>);
