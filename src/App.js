f9ce2442c35488c08cae47ad17b633f6db3ea13b
emilcecarlisa <emilcecarlisa@gmail.com>
2018-06-20 16:25:09 -0700

    Adjusting the rental post request

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const CUSTOMER_URL =
 'http://localhost:3000/customers';
 
 const CHECKOUT_URL =
-'http://localhost:3000/rentals'
+'http://localhost:3000/rentals';
 
 
 class App extends Component {
@@ -41,7 +41,6 @@ class App extends Component {
       this.setState({movies: data})
     })
   }
-
 
   onClickMovie = (movieInfo) => {
     return (event) => {
@@ -66,11 +65,11 @@ class App extends Component {
     )
   }
 
-  checkOutNewRental = (event)=> {
+  checkOutNewRental = (event) => {
     var due_date = new Date();
     var numberofDaysToAdd = 3;
     due_date.setDate(due_date.getDate() + numberofDaysToAdd);
-
+    let title = this.state.selectedMovie;
 
     let checkoutObj ={
       movie_id: this.state.selectedMovie,
@@ -78,20 +77,14 @@ class App extends Component {
       due_date: due_date
     }
 
-    console.log(checkoutObj)
-    let title = this.state.selectedMovie;
+    console.log('in checkOutRental', checkoutObj)
+    // POST /rentals/:title/check-out
+    axios.post(CHECKOUT_URL+`/${title}/check-out`, checkoutObj)
+    .then((response) => {
+      console.log('things happening',response)
+    })
+  };
 
-    return (event) => {
-      event.preventDefault()
-
-      axios.post(CHECKOUT_URL+`${title}/check-out` , checkoutObj)
-      .then((response) => {
-        console.log('things happening')
-
-
-      })
-    };
-  }
   componentDidMount(){
     axios.get(CUSTOMER_URL)
     .then((response) => {
@@ -108,9 +101,9 @@ class App extends Component {
   render() {
     const custId = ({ match }) => (
       <div>
-        <Route path ={match.url + '/:id'} render={() => (
-          <h1>Customer #11111</h1>
-        )} />
+      <Route path ={match.url + '/:id'} render={() => (
+        <h1>Customer #11111</h1>
+      )} />
       </div>
     )
 
@@ -120,64 +113,64 @@ class App extends Component {
 
       console.log(movieInfo);
       return <div>
-        <img src={movieInfo.poster_path} alt="movie image"/>
-        <p>{movieInfo.title}</p>
-        <p>{movieInfo.release_date}</p>
+      <img src={movieInfo.poster_path} alt="movie image"/>
+      <p>{movieInfo.title}</p>
+      <p>{movieInfo.release_date}</p>
 
-        <button onClick={this.onClickMovie(movieInfo)}>Add to Rental Library</button>
+      <button onClick={this.onClickMovie(movieInfo)}>Add to Rental Library</button>
       </div>
     })
 
     return (
       <Router>
+      <div>
+      <Link to='/'>Home</Link><br />
+      <Link to='/customers'>Customers</Link><br />
+      <Link to='/library'>Movie Library</Link>
+      <section>
+      <div>
+      Selected Customer:
+      {this.state.selectedCustomer}
+      </div>
+      <div>
+      Selected Movie: {this.state.selectedMovie}
+      </div>
+      <div>
+      <button onClick={this.checkOutNewRental}> Checkout New Rental</button>
+      </div>
+      </section>
+      <Route exact={true} path ="/" render={() => (
         <div>
-          <Link to='/customers'>Customers          </Link>
-          <Link to='/'>Home          </Link>
-          <Link to='/library'>Movie Library        </Link>
-          <section>
-            <div>
-              Selected Customer:
-              {this.state.selectedCustomer}
-            </div>
-            <div>
-              Selected Movie {this.state.selectedMovie}
-            </div>
-            <div>
-              <button onClick={this.checkOutNewRental}> Checkout New Rental</button>
-            </div>
-          </section>
-          <Route exact={true} path ="/" render={() => (
-            <div>
-              <h1>Welcome to your local Video Store</h1>
-              <Movie getMoviesCallback={this.getMovies}/>
-              {searchResults}
-            </div>
-          )} />
+        <h1>Welcome to your local Video Store</h1>
+        <Movie getMoviesCallback={this.getMovies}/>
+        {searchResults}
+        </div>
+      )} />
 
-          <Route path="/movie" render={() => (
-            <h1>Search Movies</h1>
-          )} />
+      <Route path="/movie" render={() => (
+        <h1>Search Movies</h1>
+      )} />
 
-          <Route path="/library" render={() => (
-            <div>
-              <h1>Library</h1>
-              <Library libraryMovies={this.state.libraryMovies} selectedMovieCallback= {this.setSelectedMovie}/>
+      <Route path="/library" render={() => (
+        <div>
+        <h1>Library</h1>
+        <Library libraryMovies={this.state.libraryMovies} selectedMovieCallback= {this.setSelectedMovie}/>
 
-            </div>
-          )} />
+        </div>
+      )} />
 
-          <Route path="/customers" render={() => (
-            <div>
-              <h1> Customers</h1>
-              <Customers customers={this.state.customers}
-                selectedCustomerCallback= {this.setSelectedCustomer} />
-              </div>
-            )} />
+      <Route path="/customers" render={() => (
+        <div>
+        <h1> Customers</h1>
+        <Customers customers={this.state.customers}
+        selectedCustomerCallback= {this.setSelectedCustomer} />
+        </div>
+      )} />
 
-          </div>
-        </Router>
-      );
-    }
+      </div>
+      </Router>
+    );
   }
+}
 
-  export default App;
+export default App;
